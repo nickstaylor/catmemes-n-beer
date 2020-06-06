7e1fb396580493fbe72e43d7acb6fc6514f82f26
Nick Taylor <nickstaylor@gmail.com>
2020-06-06 08:49:29 -0600

    Create new files for components

---
+++ src/BreweryContainer/BreweryContainer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import './BreweryContainer.css'
+import {} from 'react-router-dom'
+// import PropTypes from 'prop-types'
+
+
+
+class BreweryContainer extends Component {
+constructor(props){
+  super(props)
+  this.state = {}
+}
+
+
+render(){
+  return (
+    <div>
+    BreweryContainer
+    </div>
+  )
+}
+
+
+}
+
+export default BreweryContainer
