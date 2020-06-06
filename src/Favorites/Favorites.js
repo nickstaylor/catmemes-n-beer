876be54d294931253c9bf8a70b8c7bb192b4e008
Nick Taylor <nickstaylor@gmail.com>
2020-06-06 08:50:09 -0600

    Create new files for components

---
+++ src/Favorites/Favorites.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import './Favorites.css'
+import {} from 'react-router-dom'
+// import PropTypes from 'prop-types'
+
+
+
+class Favorites extends Component {
+constructor(props){
+  super(props)
+  this.state = {}
+}
+
+
+render(){
+  return (
+    <div>
+    Favorites
+    </div>
+  )
+}
+
+
+}
+
+export default Favorites
