0adb62a5217456aa2aa6cd67e9a26c5951a2e5e5
Nick Taylor <nickstaylor@gmail.com>
2020-06-06 19:59:15 -0600

    Move js files into folders.  Update methods on each file as needed

---
+++ src/components/Header/Header.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
+// import PropTypes from "prop-types";
+
+const Header = (props) => {
+  let favorites = 0
+  let boredActivities = props.favoriteBoredActivity
+  let dadJokes = props.favoriteDadJokes
+  let catMemes = props.favoriteCatMemes
+  let breweries = props.favoriteBreweries
+  let sum = favorites + catMemes + breweries + dadJokes + boredActivities
+  // console.log(props);
+  // console.log('sum', sum);
+  return (
+    <header>
+    <div className="logo">
+    <p>The Thirsty Cat Dad's Escape.</p>
+    <p className="header-username"> Meow <span>{props.user}</span>!</p>
+    </div>
+      <nav className="nav-container">
+        <NavLink to="/landing" className="nav">
+          {" "}
+           Home
+          {" "}
+        </NavLink>
+        {
+        <NavLink to="/favorites" className="nav" onClick={props.loadFavorites}>
+          Favorites (
+          {!sum
+            ? "0"
+            : `${sum}`}
+          ){" "}
+        </NavLink>
+      }
+        <NavLink to="/" exact className="nav" onClick={props.removeUser}>
+          {" "}
+          Sign Out{" "}
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+// Header.propTypes = {
+//   removeUser: PropTypes.func,
+//   numberofFavorites: PropTypes.array,
+//   loadFavorites: PropTypes.func,
+// };
+export default Header;
---
+++ src/components/Login/Login.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import "./Login.css";
+import { Redirect } from 'react-router-dom'
+// import PropTypes from 'prop-types'
+
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "",
+      zipCode: "80202",
+      completedForm: false,
+      loginError: ''
+    };
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  validateUser = (event) => {
+    event.preventDefault()
+
+    if (this.state.userName === '' || this.state.zipCode.length !== 5){
+      this.setState({loginError: "One moment Daddy-O.  Please complete the inputs"})
+    } else {
+    this.setState({completedForm: true,
+                  loginError: ''})
+    const user = {user: this.state.userName, zipCode: this.state.zipCode}
+    this.props.addUser(user)
+    }
+  }
+
+
+  render(){
+    return (
+      <section className="login-form-background">
+        <div className="login-content">
+            <h1 className="main-title">A Little Getaway for Kitty-Loving Cheesy Beer Dads</h1>
+          <form className="login-form" onSubmit = {(e)=> this.validateUser(e)}>
+            {
+              this.state.loginError
+              && <p className="login-error">{this.state.loginError}</p>
+            }
+            <div className="login-form-item">
+              <label htmlFor="userName">Name</label>
+              <input
+                type="text"
+                name="userName"
+                placeholder="name"
+                value={this.state.userName}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="login-form-item">
+              <label htmlFor="zipCode">Zip Code</label>
+              <input
+                type="text"
+                name="zipCode"
+                maxLength="5"
+                placeholder="80202"
+                value={this.state.zipCode}
+                onChange={this.handleChange}
+              />
+              <label  className="zipcode" htmlFor="zipCode">(Zip Code? For Beer! You'll See) </label>
+            </div>
+            <button className="login-btn">MEOW!</button>
+          </form>
+          {this.state.completedForm && <Redirect to="/landing" />}
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Login
