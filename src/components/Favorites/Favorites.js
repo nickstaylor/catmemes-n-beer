99b94b7f45f2199c0577177abb00522ca19543d6
Nick Taylor <nickstaylor@gmail.com>
2020-06-07 14:16:44 -0600

    Favorites parent container layout and styling

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,24 +1,48 @@
 import React, { Component } from 'react'
 import './Favorites.css'
-import {} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 // import PropTypes from 'prop-types'
 
-
-
-class Favorites extends Component {
-constructor(props){
-  super(props)
-  this.state = {}
-}
-
-
-render(){
+const Favorites = (props)=> {
+  let boredActivities = props.favoriteBoredActivity
+  let dadJokes = props.favoriteDadJokes
+  let catMemes = props.favoriteCatMemes
+  let breweries = props.favoriteBreweries
+  let sum = catMemes + breweries + dadJokes + boredActivities
   return (
-    <div>
-    Favorites
+    !sum ? <div>No Favorites yet!  Explore the site!</div> :
+    <div className ="favorites-container">
+      <div className="favorites-box">
+        <p>My Cat Memes</p>
+        <Link to='/favorites/catmemes'>
+        <button className="topic-button">Saved Cat Memes ({catMemes})</button>
+        </Link>
+      </div>
+
+      <div className="favorites-box">
+        <p>My Dad Jokes</p>
+        <Link to='/favorites/dadjokes'>
+        <button className="topic-button">Saved Dad Jokes ({dadJokes})</button>
+        </Link>
+      </div>
+
+      <div className="favorites-box">
+        <p>My Bored Kids Advice</p>
+        <Link to='/favorites/boredactivities'>
+        <button className="topic-button">Saved Bored Advice({boredActivities})</button>
+        </Link>
+      </div>
+
+      <div className="favorites-box">
+        <p>My Breweries</p>
+        <Link to='/favorites/breweries'>
+        <button className="topic-button" onClick={props.loadFavoriteBreweries}>Saved Breweries ({breweries})</button>
+        </Link>
+      </div>
     </div>
+
   )
-}
+
 
 
 }
