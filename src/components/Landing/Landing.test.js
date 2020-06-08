989d7ae5d2010f42235119fe90937a3d84f65890
Nick Taylor <nickstaylor@gmail.com>
2020-06-08 10:22:23 -0600

    Write landing unit tests.  Tests are passing.

---
+++ src/components/Landing/Landing.test.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { BrowserRouter, MemoryRouter } from 'react-router-dom'
+import Landing from "./Landing";
+import MutationObserver from '@sheerun/mutationobserver-shim'
+window.MutationObserver = MutationObserver
+
+
+
+describe("Landing", ()=>{
+  it("should render 4 topics on the page", () => {
+
+    const topics = [{name: 'Create a Cat Meme!', topic: 'catmemes'},
+    {name: 'Cheesy Dad Jokes!', topic: 'dadjokes'},
+    {name: 'Bored Kids?', topic: 'bored'},
+    {name: 'Find Me a Local Brewery, NOW!', topic:'breweries' }]
+    const router = (<BrowserRouter>
+                    <Landing topics={topics} />
+                    </BrowserRouter>)
+
+    const { getByText, getAllByRole, debug } = render(router)
+
+    const images = getAllByRole('img');
+    expect(images).toHaveLength(4)
+    expect(getByText('Bored Kids?')).toBeInTheDocument()
+  })
+
+  it("should take user to specific topic when button clicked", async () => {
+    const topics = [{name: 'Create a Cat Meme!', topic: 'catMemes'},
+    {name: 'Cheesy Dad Jokes!', topic: 'dadjokes'},
+    {name: 'Bored Kids?', topic: 'bored'},
+    {name: 'Find Me a Local Brewery, NOW!', topic:'breweries' }]
+    const router = (<BrowserRouter>
+                    <Landing topics={topics} />
+                    </BrowserRouter>)
+
+    const { getByText, getAllByRole, getByTestId, debug } = render(router)
+
+    const button = getByTestId('Cheesy Dad Jokes!');
+    fireEvent.click(button)
+    // const dadPage = await waitFor(() => getByText('Enjoy a Dad Joke! Tell Your Friends!'))
+    // expect(dadPage).toBeInTheDocument();
+    expect(window.location.href).toBe("http://localhost/dadjokes");
+  })
+})
