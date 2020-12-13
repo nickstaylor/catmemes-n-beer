e505ae8288bcfa9205a0502911064abb7ac89758
Nick Taylor <nickstaylor@gmail.com>
2020-12-13 09:22:56 -0700

    Changes made to catfacts object and brewery object as API return data changed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,31 +1,35 @@
-import React, { Component } from 'react';
-import './App.css';
-import { catFacts, getCoordinates, getBreweries, getBreweryPhotos } from "../../apiCalls.js"
+import React, { Component } from "react";
+import "./App.css";
+import {
+  catFacts,
+  getCoordinates,
+  getBreweries,
+  getBreweryPhotos,
+} from "../../apiCalls.js";
 import { Switch, Route, Redirect } from "react-router-dom";
-import Login from '../Login/Login'
-import Header from '../Header/Header'
-import Landing from '../Landing/Landing'
-import Footer from '../Footer/Footer'
-import CatMemes from '../CatMemes/CatMemes'
-import Bored from '../Bored/Bored'
-import Favorites from '../Favorites/Favorites'
-import BreweryContainer from '../BreweryContainer/BreweryContainer'
-import BreweryDetails from '../BreweryDetails/BreweryDetails'
-import DadJoke from '../DadJoke/DadJoke'
-import FavoriteCatMemes from '../FavoriteCatMemes/FavoriteCatMemes'
-import FavoriteDadJokes from '../FavoriteDadJokes/FavoriteDadJokes'
-import FavoriteBored from '../FavoriteBored/FavoriteBored'
-import FavoriteBreweries from '../FavoriteBreweries/FavoriteBreweries'
-import dadJokeImg from '../../images/DadJokes.jpg'
-import boredKids from '../../images/BoredKids.jpg'
-import catMeme from '../../images/catMeme.jpeg'
-import breweryImg from '../../images/Brewery.jpg'
-import stockPhoto from '../../images/stockPhoto.jpg'
-
+import Login from "../Login/Login";
+import Header from "../Header/Header";
+import Landing from "../Landing/Landing";
+import Footer from "../Footer/Footer";
+import CatMemes from "../CatMemes/CatMemes";
+import Bored from "../Bored/Bored";
+import Favorites from "../Favorites/Favorites";
+import BreweryContainer from "../BreweryContainer/BreweryContainer";
+import BreweryDetails from "../BreweryDetails/BreweryDetails";
+import DadJoke from "../DadJoke/DadJoke";
+import FavoriteCatMemes from "../FavoriteCatMemes/FavoriteCatMemes";
+import FavoriteDadJokes from "../FavoriteDadJokes/FavoriteDadJokes";
+import FavoriteBored from "../FavoriteBored/FavoriteBored";
+import FavoriteBreweries from "../FavoriteBreweries/FavoriteBreweries";
+import dadJokeImg from "../../images/DadJokes.jpg";
+import boredKids from "../../images/BoredKids.jpg";
+import catMeme from "../../images/catMeme.jpeg";
+import breweryImg from "../../images/Brewery.jpg";
+import stockPhoto from "../../images/stockPhoto.jpg";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       catFacts: [],
       favoriteCatMemes: [],
@@ -35,105 +39,124 @@ class App extends Component {
       favoriteBoredActivities: [],
       breweries: [],
       randomFact: {},
-      user: '',
-      zipCode: '',
+      user: "",
+      zipCode: "",
       coordinates: {},
       zipCodeError: false,
-      topics: [{name: 'Create a Cat Meme!', photo: catMeme, topic: 'catMemes'},
-               {name: 'Cheesy Dad Jokes!', photo: dadJokeImg, topic: 'dadjokes'},
-               {name: 'Bored Kids?', photo: boredKids, topic: 'bored'},
-               {name: 'Find Me a Local Brewery, NOW!', photo: breweryImg, topic:'breweries' }]
-    }
+      topics: [
+        { name: "Create a Cat Meme!", photo: catMeme, topic: "catMemes" },
+        { name: "Cheesy Dad Jokes!", photo: dadJokeImg, topic: "dadjokes" },
+        { name: "Bored Kids?", photo: boredKids, topic: "bored" },
+        {
+          name: "Find Me a Local Brewery, NOW!",
+          photo: breweryImg,
+          topic: "breweries",
+        },
+      ],
+    };
   }
 
   componentDidMount = async () => {
     //cat facts fetch
     const catFactsApi = await catFacts();
-    let facts = catFactsApi.map(fact => {
-      return {text: fact.text, upVotes: fact.upvotes, id: fact._id, voted: false}
-    })
-    let newFacts = facts.filter(fact => fact.id !== '5ece9fa9a0d2ec00178ae28e')
-    this.setState({ catFacts: newFacts,
-                    randomFact: newFacts[0] })
-  }
+    let facts = catFactsApi.map((fact) => {
+      return {
+        text: fact.text,
+        upVotes: 0,
+        id: fact._id,
+        voted: false,
+      };
+    });
+    let newFacts = facts.filter(
+      (fact) => fact.id !== "5ece9fa9a0d2ec00178ae28e"
+    );
+    this.setState({ catFacts: newFacts, randomFact: newFacts[0] });
+  };
 
   getCoordinatesFromZip = async (zipCode) => {
     //zipCode fetch
-    let fetchedZipCode = await getCoordinates(zipCode)
-    if (fetchedZipCode.length){
-      this.setState({coordinates: fetchedZipCode[0].geometry.location})
+    let fetchedZipCode = await getCoordinates(zipCode);
+    if (fetchedZipCode.length) {
+      this.setState({ coordinates: fetchedZipCode[0].geometry.location });
     } else {
-      this.setState({zipCodeError: true})
+      this.setState({ zipCodeError: true });
     }
-    this.getBrewerySpots()
-  }
+    this.getBrewerySpots();
+  };
 
   getBrewerySpots = async () => {
-    let breweries
-    if(this.state.zipCodeError){
-      let coordinates = {lat: "39.7541032", lng: "-105.0002242"}
-      breweries = await getBreweries(coordinates)
+    let breweries;
+    if (this.state.zipCodeError) {
+      let coordinates = { lat: "39.7541032", lng: "-105.0002242" };
+      breweries = await getBreweries(coordinates);
     } else {
-      breweries = await getBreweries(this.state.coordinates)
+      breweries = await getBreweries(this.state.coordinates);
     }
-    let photo
-    let breweryArray = []
-    breweries.forEach(brewery => {
-      if (brewery.photos === undefined){
-        photo = stockPhoto
+    let photo;
+    let breweryArray = [];
+    breweries.forEach((brewery) => {
+      if (brewery.photos === undefined) {
+        photo = stockPhoto;
       } else {
-        photo = getBreweryPhotos(brewery.photos[0].photo_reference)
+        photo = getBreweryPhotos(brewery.photos[0].photo_reference);
       }
-      breweryArray.push(
-        {
-          name: brewery.name,
-          id: brewery.id,
-          address: brewery.vicinity,
-          rating: brewery.rating,
-          photo: photo,
-          coordinates: brewery.geometry.location,
-          placeId: brewery.place_id,
-          favorite: false,
-          reviews: []
-        }
-      )
-    })
-    this.setState({ breweries: breweryArray })
-  }
+      breweryArray.push({
+        name: brewery.name,
+        id: brewery.place_id,
+        address: brewery.vicinity,
+        rating: brewery.rating,
+        photo: photo,
+        coordinates: brewery.geometry.location,
+        favorite: false,
+        reviews: [],
+      });
+    });
+    this.setState({ breweries: breweryArray });
+  };
 
   addUser = (user) => {
-    this.setState ({ user: user.user,
-                     zipCode: user.zipCode })
-    this.getCoordinatesFromZip(+user.zipCode)
-  }
+    this.setState({ user: user.user, zipCode: user.zipCode });
+    this.getCoordinatesFromZip(+user.zipCode);
+  };
 
   removeUser = () => {
-    this.setState({ user: '',
-                   favoriteCatMemes: [],
-                   favoriteDadJokes: [],
-                   favoriteBreweries: [],
-                   favoriteBoredActivities: [],
-                   favoriteBreweryIDs: [] })
-  }
+    this.setState({
+      user: "",
+      favoriteCatMemes: [],
+      favoriteDadJokes: [],
+      favoriteBreweries: [],
+      favoriteBoredActivities: [],
+      favoriteBreweryIDs: [],
+    });
+  };
 
   favoriteCatMeme = (object) => {
-      this.setState({favoriteCatMemes: [...this.state.favoriteCatMemes, object]})
-  }
+    this.setState({
+      favoriteCatMemes: [...this.state.favoriteCatMemes, object],
+    });
+  };
 
   favoriteDadJoke = (object) => {
-      this.setState({favoriteDadJokes: [...this.state.favoriteDadJokes, object]})
-  }
+    this.setState({
+      favoriteDadJokes: [...this.state.favoriteDadJokes, object],
+    });
+  };
 
   favoriteBoredActivity = (activity) => {
-    this.setState({favoriteBoredActivities: [...this.state.favoriteBoredActivities, activity]})
-  }
+    this.setState({
+      favoriteBoredActivities: [
+        ...this.state.favoriteBoredActivities,
+        activity,
+      ],
+    });
+  };
 
   deleteFavorite = (id, name) => {
-    let filtered = this.state[name].filter(favorite=>{
-      return favorite.id !== id
-    })
-    this.setState({[name]: filtered})
-  }
+    let filtered = this.state[name].filter((favorite) => {
+      return favorite.id !== id;
+    });
+    this.setState({ [name]: filtered });
+  };
 
   loadFavoriteBreweries = () => {
     let favorites = this.state.breweries.reduce((acc, brewery) => {
@@ -142,144 +165,194 @@ class App extends Component {
           acc.push(brewery);
         }
       });
-    return acc;
-  }, []);
-  this.setState({ favoriteBreweries: favorites });
-  }
+      return acc;
+    }, []);
+    this.setState({ favoriteBreweries: favorites });
+  };
 
   updatefavoriteBreweries = (newFavorites) => {
-    this.setState({favoriteBreweries: newFavorites})
-  }
+    this.setState({ favoriteBreweries: newFavorites });
+  };
 
   toggleFavoriteBrewery = (id) => {
-    if (!this.state.favoriteBreweryIDs.includes(id)){
-      this.setState({favoriteBreweryIDs: [...this.state.favoriteBreweryIDs, id]})
+    if (!this.state.favoriteBreweryIDs.includes(id)) {
+      this.setState({
+        favoriteBreweryIDs: [...this.state.favoriteBreweryIDs, id],
+      });
     } else {
-      let newFavorites = this.state.favoriteBreweryIDs.filter(favorite=>{
-        return favorite !== id
-      })
-      this.setState({favoriteBreweryIDs: newFavorites})
+      let newFavorites = this.state.favoriteBreweryIDs.filter((favorite) => {
+        return favorite !== id;
+      });
+      this.setState({ favoriteBreweryIDs: newFavorites });
     }
-    this.loadFavoriteBreweries()
-  }
+    this.loadFavoriteBreweries();
+  };
 
-  getNewFact = ()=> {
-    let anotherRandomNumber = Math.floor(Math.random() * this.state.catFacts.length);
-    let randomFact = this.state.catFacts[anotherRandomNumber]
-    this.setState({randomFact: randomFact})
-  }
+  getNewFact = () => {
+    let anotherRandomNumber = Math.floor(
+      Math.random() * this.state.catFacts.length
+    );
+    let randomFact = this.state.catFacts[anotherRandomNumber];
+    this.setState({ randomFact: randomFact });
+  };
 
   randomFactVote = (id) => {
-    let upVote = this.state.catFacts.find(fact=>fact.id === id)
-    if (!this.state.randomFact.voted){
-      upVote.upVotes++
-      upVote.voted = true
-    } else { upVote.upVotes--
-             upVote.voted = false
-           }
-    this.setState({randomFact: upVote})
-  }
-
-
-  render(){
-  return (
-    <div className="App">
-    {this.state.user &&
-      <Header
-      loadFavorites={this.loadFavorites}
-      favoriteBoredActivity={this.state.favoriteBoredActivities.length}
-      favoriteDadJokes={this.state.favoriteDadJokes.length}
-      favoriteCatMemes={this.state.favoriteCatMemes.length}
-      favoriteBreweries={this.state.favoriteBreweryIDs.length}
-      user={this.state.user}
-      removeUser={this.removeUser}
-      />}
-    <Switch>
-
-    <Route path="/" exact render={() =>
-      <Login addUser={this.addUser} />}
-    />
-
-    {this.state.user ?
-    <Route path="/landing" exact render={() =>
-      <Landing  topics={this.state.topics} />}
-    /> :
-    <Redirect to="/" />
+    let upVote = this.state.catFacts.find((fact) => fact.id === id);
+    if (!this.state.randomFact.voted) {
+      upVote.upVotes++;
+      upVote.voted = true;
+    } else {
+      upVote.upVotes--;
+      upVote.voted = false;
     }
+    this.setState({ randomFact: upVote });
+  };
 
-    <Route path="/catMemes" exact render={() =>
-      <CatMemes favoriteCatMeme={this.favoriteCatMeme} />}
-    />
+  render() {
+    return (
+      <div className="App">
+        {this.state.user && (
+          <Header
+            loadFavorites={this.loadFavorites}
+            favoriteBoredActivity={this.state.favoriteBoredActivities.length}
+            favoriteDadJokes={this.state.favoriteDadJokes.length}
+            favoriteCatMemes={this.state.favoriteCatMemes.length}
+            favoriteBreweries={this.state.favoriteBreweryIDs.length}
+            user={this.state.user}
+            removeUser={this.removeUser}
+          />
+        )}
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={() => <Login addUser={this.addUser} />}
+          />
 
-    <Route path="/favorites" exact render={() =>
-      <Favorites
-      topics={this.state.topics}
-      loadFavoriteBreweries={this.loadFavoriteBreweries}
-      favoriteBoredActivity={this.state.favoriteBoredActivities.length}
-      favoriteDadJokes={this.state.favoriteDadJokes.length}
-      favoriteCatMemes={this.state.favoriteCatMemes.length}
-      favoriteBreweries={this.state.favoriteBreweryIDs.length} />}
-    />
+          {this.state.user ? (
+            <Route
+              path="/landing"
+              exact
+              render={() => <Landing topics={this.state.topics} />}
+            />
+          ) : (
+            <Redirect to="/" />
+          )}
 
-    <Route path="/breweries" exact render={() =>
-      <BreweryContainer
-      favoriteBreweryIDs={this.state.favoriteBreweryIDs}
-      breweries={this.state.breweries}
-      zipCodeError={this.state.zipCodeError}
-      zipCode={this.state.zipCode}
-      toggleFavoriteBrewery={this.toggleFavoriteBrewery}
-      />}
-    />
+          <Route
+            path="/catMemes"
+            exact
+            render={() => <CatMemes favoriteCatMeme={this.favoriteCatMeme} />}
+          />
 
-    <Route path="/breweries/:name" exact render={() => <BreweryDetails />}
-    />
+          <Route
+            path="/favorites"
+            exact
+            render={() => (
+              <Favorites
+                topics={this.state.topics}
+                loadFavoriteBreweries={this.loadFavoriteBreweries}
+                favoriteBoredActivity={
+                  this.state.favoriteBoredActivities.length
+                }
+                favoriteDadJokes={this.state.favoriteDadJokes.length}
+                favoriteCatMemes={this.state.favoriteCatMemes.length}
+                favoriteBreweries={this.state.favoriteBreweryIDs.length}
+              />
+            )}
+          />
 
-    <Route path ="/bored" exact render={() =>
-      <Bored favoriteBoredActivity={this.favoriteBoredActivity} />}
-    />
+          <Route
+            path="/breweries"
+            exact
+            render={() => (
+              <BreweryContainer
+                favoriteBreweryIDs={this.state.favoriteBreweryIDs}
+                breweries={this.state.breweries}
+                zipCodeError={this.state.zipCodeError}
+                zipCode={this.state.zipCode}
+                toggleFavoriteBrewery={this.toggleFavoriteBrewery}
+              />
+            )}
+          />
 
-    <Route path ="/dadjokes" exact render={() =>
-      <DadJoke favoriteDadJoke={this.favoriteDadJoke} />}
-    />
+          <Route
+            path="/breweries/:name"
+            exact
+            render={() => <BreweryDetails />}
+          />
 
-    <Route path ="/favorites/catmemes" exact render={() =>
-      <FavoriteCatMemes
-        name={'favoriteCatMemes'}
-        favoriteCatMemes={this.state.favoriteCatMemes}
-        deleteFavorite={this.deleteFavorite}/>}
-    />
+          <Route
+            path="/bored"
+            exact
+            render={() => (
+              <Bored favoriteBoredActivity={this.favoriteBoredActivity} />
+            )}
+          />
 
-    <Route path ="/favorites/dadjokes" exact render={() =>
-       <FavoriteDadJokes
-        name={'favoriteDadJokes'}
-        favoriteDadJokes={this.state.favoriteDadJokes}
-        deleteFavorite={this.deleteFavorite}/>}
-    />
+          <Route
+            path="/dadjokes"
+            exact
+            render={() => <DadJoke favoriteDadJoke={this.favoriteDadJoke} />}
+          />
 
-    <Route path ="/favorites/boredactivities" exact render={() =>
-       <FavoriteBored
-       name={'favoriteBoredActivities'}
-       favoriteBoredActivities={this.state.favoriteBoredActivities}
-       deleteFavorite={this.deleteFavorite} />}
-    />
+          <Route
+            path="/favorites/catmemes"
+            exact
+            render={() => (
+              <FavoriteCatMemes
+                name={"favoriteCatMemes"}
+                favoriteCatMemes={this.state.favoriteCatMemes}
+                deleteFavorite={this.deleteFavorite}
+              />
+            )}
+          />
 
-    <Route path ="/favorites/breweries" exact render={() =>
-       <FavoriteBreweries
-       toggleFavoriteBrewery={this.toggleFavoriteBrewery}
-       favoriteBreweries={this.state.favoriteBreweries}
-       updatefavoriteBreweries={this.updatefavoriteBreweries}/>}
-    />
+          <Route
+            path="/favorites/dadjokes"
+            exact
+            render={() => (
+              <FavoriteDadJokes
+                name={"favoriteDadJokes"}
+                favoriteDadJokes={this.state.favoriteDadJokes}
+                deleteFavorite={this.deleteFavorite}
+              />
+            )}
+          />
 
-    </Switch>
-    {this.state.user &&
-      <Footer
-      getNewFact={this.getNewFact}
-      randomFact={this.state.randomFact}
-      randomFactVote={this.randomFactVote}
-      />}
+          <Route
+            path="/favorites/boredactivities"
+            exact
+            render={() => (
+              <FavoriteBored
+                name={"favoriteBoredActivities"}
+                favoriteBoredActivities={this.state.favoriteBoredActivities}
+                deleteFavorite={this.deleteFavorite}
+              />
+            )}
+          />
 
-    </div>
-  )
+          <Route
+            path="/favorites/breweries"
+            exact
+            render={() => (
+              <FavoriteBreweries
+                toggleFavoriteBrewery={this.toggleFavoriteBrewery}
+                favoriteBreweries={this.state.favoriteBreweries}
+                updatefavoriteBreweries={this.updatefavoriteBreweries}
+              />
+            )}
+          />
+        </Switch>
+        {this.state.user && (
+          <Footer
+            getNewFact={this.getNewFact}
+            randomFact={this.state.randomFact}
+            randomFactVote={this.randomFactVote}
+          />
+        )}
+      </div>
+    );
   }
 }
 
